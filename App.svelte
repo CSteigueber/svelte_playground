a12e21134ba757f5170a86793b5e4e8491ffe2da
Claas Steigüber <56023114+CSteigueber@users.noreply.github.com>
2020-03-16 10:36:42 +0100

    using $:

--- App.svelte
+++ App.svelte
@@ -1,5 +1,16 @@
 <script>
 	let name = 'evil overlord';
+	let count=0;
+	$: if (count>10) {
+		alert(`Mr ${name.toUpperCase()}, you already clicked 10 times. Let us reset the counter!`);
+		count=0;
+	}
+	function handleClick(){
+		count++;
+	}
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+<button on:click={handleClick}>
+You, {name.toUpperCase()} clicked {count} {count===1? 'time':'times'}
+</button>
