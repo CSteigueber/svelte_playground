50d5912d4998377094136dee258b3211125bf399
Claas Steigüber <56023114+CSteigueber@users.noreply.github.com>
2020-03-16 14:12:34 +0100

    playing with bindings

--- App.svelte
+++ App.svelte
@@ -1,23 +1,11 @@
 <script>
 	import Nested from './Nested.svelte';
 	let name = 'evil overlord';
-	let count=0;
-	let numbers=[1,2];
-	$: if (count>10) {
-		alert(`Mr ${name.toUpperCase()}, you already clicked 10 times. Let us reset the counter!`);
-		count=0;
-	}
-	function handleClick(){
-		numbers[numbers.length] =numbers.length + 1;
-		count++;
-	}
-	$: sum=numbers.reduce((t,n)=> t+n,0);
+	let value=name
+	$:name=value
+
 </script>
-
-<p>{numbers.join(' + ')} = {sum} </p>
-
-<h1>Hello {name.toUpperCase()}!</h1>
-<button on:click={handleClick}>
-You, {name.toUpperCase()} clicked {count} {count===1? 'time':'times'}
-</button>
 <Nested lord={name}/>
+Do you wish to change your name dear Mr {name.toUpperCase()}?
+<textarea bind:value></textarea>
+
